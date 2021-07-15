8be79e5c578beae6ea36fdf1946fc47baf20681b
keyeao <86913863+keyeao@users.noreply.github.com>
2021-07-15 21:06:50 +0900

    Add files via upload

--- colors.js
+++ colors.js
@@ -1,11 +1,11 @@
 
 function setColor(color){
-  var alist = document.querySelectorAll('a');
-  var i = 0;
-  while(i<alist.length){
-  alist[i].style.color = color;
-  i = i+1;
-  }
+ var alist = document.querySelectorAll('a');
+ var i = 0;
+ while(i<alist.length){
+ alist[i].style.color = color;
+ i = i+1;
+ }
 }
 function nightDayHandler(self){
 var target = document.querySelector('body')
@@ -15,11 +15,11 @@ target.style.color = 'white';
 self.value = 'day';
 setColor('powderblue');
 
-  }
+ }
 else{
 target.style.backgroundColor = 'white';
 target.style.color = 'black';
 self.value = 'night';
 setColor('blue');
-  }
+ }
 }
